8f4e724f71b5ba1cb17b7ec483a8f712102a5b1a
Matthew Gieger <mrgieger@gmail.com>
2021-02-28 17:53:00 -0800

    Refactor core.search and hex_search modules

--- lib/core/seach.ex
+++ lib/core/seach.ex
@@ -4,12 +4,35 @@ defmodule HexSearch.Core.Search do
   def base_url(), do: @hex_doc_base_url
 
   def open_url(url) do
-    System.cmd("open", [url])
+    System.cmd("open", [@hex_doc_base_url <> url])
   end
 
-  def search(search_string) do
-    open_url(@hex_doc_base_url <> search_string)
+  def search({_module, _function} = args) do
+    args
+    |> build_search_url()
+    |> open_url()
   end
 
-  def hex_docs(), do: open_url(@hex_doc_base_url)
+  def search(search_string) when is_binary(search_string) do
+    open_url(search_string)
+  end
+
+  defp build_search_url({module, nil}) do
+    parse_module(module) <> ".html"
+  end
+
+  defp build_search_url(args) do
+    {module, function} = args
+
+    parse_module(module) <> ".html##{function}"
+  end
+
+  defp parse_module(module) do
+    module
+    |> String.split(".", trim: true)
+    |> case do
+      [module] -> "#{String.downcase(module)}/#{String.capitalize(module)}"
+      [module, sub_module] -> "#{String.downcase(module)}/#{String.capitalize(module)}.#{String.capitalize(sub_module)}"
+    end
+  end
 end
